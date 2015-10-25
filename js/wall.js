db8143c3b869169705edf9599f0a821b9b4e10e9
Richard Schneider <richard@schneiderbox.net>
2015-10-25 15:33:38 -0500

    add debug mode that renders walls

--- js/wall.js
+++ js/wall.js
@@ -4,6 +4,8 @@ function Wall(x, y, width, height, options){
 	options = options || {};
 	if(options.isStatic === undefined) options.isStatic = true;
 
+	this.width = width;
+	this.height = height;
 	this.body = Matter.Bodies.rectangle(
 		x,
 		y,
@@ -12,4 +14,17 @@ function Wall(x, y, width, height, options){
 		options
 	);
 	Matter.World.add(engine.world, this.body);
+	entities.push(this);
+}
+
+Wall.prototype.render = function(){
+	ctx.save();
+	ctx.translate(
+		-player.body.position.x + canvas.width/2 + this.body.position.x,
+		-player.body.position.y + canvas.height/2 + this.body.position.y
+	);
+	ctx.rotate(this.body.angle);
+	ctx.fillStyle = '#822';
+	ctx.fillRect(-this.width/2, -this.height/2, this.width, this.height);
+	ctx.restore();
 }
